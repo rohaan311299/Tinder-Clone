ec5bf57943291d71900cd5bad362bc66db1f9f34
Rohan Kacheria <51020166+rohaan311299@users.noreply.github.com>
2021-06-27 12:35:28 +0530

    Update server.js

--- tinder-backend/server.js
+++ tinder-backend/server.js
@@ -6,7 +6,7 @@ import Cors from "cors";
 // App Config
 const app = express();
 const port=process.env.PORT || 8001;
-const connection_url='mongodb+srv://Rohan:rmk9930042066@cluster0.ts9kw.mongodb.net/tinderDB?retryWrites=true&w=majority'
+const connection_url='mongodb+srv://<username>:<password>@cluster0.ts9kw.mongodb.net/<DBNAME>?retryWrites=true&w=majority'
 
 // Middlewares
 app.use(express.json());
